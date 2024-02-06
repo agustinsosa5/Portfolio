108bd19a7e4c9dc443b6d6d8ac009b94e21e3d92
Agustin Sosa <agustinsosa2563@gmail.com>
2024-02-05 23:53:30 -0300

    Se mueve Carpeta Icons y correccion de puerto en package.json

--- new-project/src/components/UI/skills.jsx
+++ new-project/src/components/UI/skills.jsx
@@ -1,6 +1,6 @@
-import "../../assets/Styles/skills.css";
+import "/src/assets/Styles/skills.css";
 import Particles from "./particles";
-import "../../assets/Styles/index.css";
+import "/src/assets/Styles/index.css";
 
 export default function Skills() {
   return (
@@ -9,15 +9,15 @@ export default function Skills() {
         <Particles />
         <h2 className="tecnologia-title">Tecnologías</h2>
         <div className="skills-grid">
-          <img src="./src/assets/icons/Boostrap.png" alt="Boostrap-icon" />
-          <img src="./src/assets/icons/CSS.png" alt="css-icon" />
-          <img src="./src/assets/icons/HTML.png" alt="html-icon" />
-          <img src="./src/assets/icons/IBM-Cloud.png" alt="ibm-icon" />
-          <img src="./src/assets/icons/NodeJS.png" alt="node-icon" />
-          <img src="./src/assets/icons/PostgreSQL.png" alt="posgres-icon" />
-          <img src="./src/assets/icons/Python.png" alt="python-icon" />
-          <img src="./src/assets/icons/React.png" alt="react-icon" />
-          <img src="./src/assets/icons/JavaScript.png" alt="javascript-icon" />
+          <img src="public/icons/Boostrap.png" alt="Boostrap-icon" />
+          <img src="public/icons/CSS.png" alt="css-icon" />
+          <img src="public/icons/HTML.png" alt="html-icon" />
+          <img src="public/icons/IBM-Cloud.png" alt="ibm-icon" />
+          <img src="public/icons/NodeJS.png" alt="node-icon" />
+          <img src="public/icons/PostgreSQL.png" alt="posgres-icon" />
+          <img src="public/icons/Python.png" alt="python-icon" />
+          <img src="public/icons/React.png" alt="react-icon" />
+          <img src="public/icons/JavaScript.png" alt="javascript-icon" />
         </div>
       </div>
     </>
