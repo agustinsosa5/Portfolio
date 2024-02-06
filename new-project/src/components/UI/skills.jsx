25682bc35de5a9584c82aabe16ce4143fdfe0703
Agustin Sosa <agustinsosa2563@gmail.com>
2024-02-06 00:18:20 -0300

    Prueba de img en skill

--- new-project/src/components/UI/skills.jsx
+++ new-project/src/components/UI/skills.jsx
@@ -1,6 +1,7 @@
 import "/src/assets/Styles/skills.css";
 import Particles from "./particles";
 import "/src/assets/Styles/index.css";
+import { SiJavascript } from "react-icons/si";
 
 export default function Skills() {
   return (
@@ -9,15 +10,16 @@ export default function Skills() {
         <Particles />
         <h2 className="tecnologia-title">Tecnologías</h2>
         <div className="skills-grid">
-          <img src="public/icons/Boostrap.png" alt="Boostrap-icon" />
-          <img src="public/icons/CSS.png" alt="css-icon" />
-          <img src="public/icons/HTML.png" alt="html-icon" />
-          <img src="public/icons/IBM-Cloud.png" alt="ibm-icon" />
-          <img src="public/icons/NodeJS.png" alt="node-icon" />
-          <img src="public/icons/PostgreSQL.png" alt="posgres-icon" />
-          <img src="public/icons/Python.png" alt="python-icon" />
-          <img src="public/icons/React.png" alt="react-icon" />
-          <img src="public/icons/JavaScript.png" alt="javascript-icon" />
+          <i><SiJavascript /></i>
+          <img src="public/icons/prueba.jpeg" alt="Boostrap-icon" />
+          <img src="/CSS.png" alt="css-icon" />
+          <img src="/HTML.png" alt="html-icon" />
+          <img src="/IBM-Cloud.png" alt="ibm-icon" />
+          <img src="/NodeJS.png" alt="node-icon" />
+          <img src="/PostgreSQL.png" alt="posgres-icon" />
+          <img src="/Python.png" alt="python-icon" />
+          <img src="/React.png" alt="react-icon" />
+          <img src="/JavaScript.png" alt="javascript-icon" />
         </div>
       </div>
     </>
