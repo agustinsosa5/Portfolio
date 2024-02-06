d92b11ff9ebaf4b57e10afabb0df7dfac9783c8a
Agustin Sosa <agustinsosa2563@gmail.com>
2024-02-06 01:02:57 -0300

    cambio ruta iconos

--- new-project/src/components/UI/skills.jsx
+++ new-project/src/components/UI/skills.jsx
@@ -10,15 +10,15 @@ export default function Skills() {
         <Particles />
         <h2 className="tecnologia-title">Tecnologías</h2>
         <div className="skills-grid">
-          <img src="src/icons/Boostrap.png" alt="Boostrap-icon" />
-          <img src="src/icons/CSS.png" alt="css-icon" />
-          <img src="src/icons/HTML.png" alt="html-icon" />
-          <img src="src/icons/IBM-Cloud.png" alt="ibm-icon" />
-          <img src="src/icons/NodeJS.png" alt="node-icon" />
-          <img src="src/icons/PostgreSQL.png" alt="posgres-icon" />
-          <img src="src/icons/Python.png" alt="python-icon" />
-          <img src="src/icons/React.png" alt="react-icon" />
-          <img src="src/icons/JavaScript.png" alt="javascript-icon" />
+          <img src="public/Boostrap.png" alt="Boostrap-icon" />
+          <img src="public/CSS.png" alt="css-icon" />
+          <img src="public/HTML.png" alt="html-icon" />
+          <img src="public/IBM-Cloud.png" alt="ibm-icon" />
+          <img src="public/NodeJS.png" alt="node-icon" />
+          <img src="public/PostgreSQL.png" alt="posgres-icon" />
+          <img src="public/Python.png" alt="python-icon" />
+          <img src="public/React.png" alt="react-icon" />
+          <img src="public/JavaScript.png" alt="javascript-icon" />
         </div>
       </div>
     </>
